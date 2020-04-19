6b7facd0f2a2970ff25aadacac170a97821b64aa
佐藤良昭 <yoshiprograming@gmail.com>
2020-04-19 20:01:38 +0900

    自販機(仮)　複数購入不可、リファクタリング必須

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,76 +1,204 @@
-require './lib/drink.rb'
-
 class VendingMachine
-  AVAILABLE_MONEY = [10, 50, 100, 500, 1000].freeze
-
-  # これで投入合計額、売上金額をメソッドで呼び出せる。
-  attr_reader :total,:sale_amount
-
-  def initialize
-    @total = 0
-    @sale_amount = 0va
-    @drink_table = {}
-    5.times { store(Drink.cola) }
+  #ここにドリンク情報をかく
+  def stock_price                                       
+      @cola = [120,5]
+      @redbull = [200,5]
+      @water = [100,5]
   end
-
-  # お金投入
-  def insert(money)
-    if VendingMachine::AVAILABLE_MONEY.include?(money)
-      @total += money
-    else
-      puts "#{money}円は使えません"
-    end
+  #ここはinfoをよぶと商品情報が出力される
+  def info(drink_name)
+      stock_price
+      stocks_down(drink_name)
+      if drink_name == "コーラ"
+          puts "コーラは#{@cola[0]}円で在庫本数は#{@cola[1]}本です！"
+      elsif drink_name == "レッドブル"
+          puts "レッドブルは#{@redbull[0]}円で在庫本数は#{@redbull[1]}本です！"
+      elsif drink_name == "水"
+          puts "水は#{@water[0]}円で在庫本数は#{@water[1]}本です！"
+      else
+          puts "そんな飲み物はありません"
+      end
   end
-
-  # 払い戻し
-  def refund
-    if @total == 0
-      puts "払い戻すお金はありません"
-    else
-      puts "#{@total}円を払い戻し"
+  #totalの初期値
+  def total_money
       @total = 0
-    end
   end
-
-  # 飲み物の補充
-  def store(drink)
-    unless @drink_table.has_key?(drink.name)
-      @drink_table[drink.name] = { price: drink.price, drinks: [] }
-    end
-    @drink_table[drink.name][:drinks] << drink
+    # お金の投入と選別
+  def insert(money)
+      avilable_money = [10, 50, 100, 500, 1000].freeze
+      if avilable_money.include?(money)
+        @total += money
+      else
+        puts "#{money}円は使えません"
+      end
   end
-
-  # 飲み物の情報
-  def stock_info
-    @drink_table.map { |name, info| [name, { price: info[:price], stock: info[:drinks].size }] }.to_h
+# お金の払い戻し
+  def refund
+      if @total == 0
+        puts "購入できません"
+      else
+        puts "#{@total}円を払い戻し"
+        @total = 0
+      end
   end
-
-  # 購入可能な飲み物の確認
-  def purchase_drink_check
-    @drink_table.select { |name,info| info[:price] <= @total && info[:drinks].size > 0 }.keys
+  #商品の選択
+  # def select_drink(drink_name)
+  #     avilable_drink = ["コーラ", "レッドブル", "水"]
+  #     if avilable_drink.include?(drink_name)
+  #         @select = 0
+  #     else
+  #         puts "そんな名前の飲み物知らない。"
+  #     end
+  # end
+  #在庫はあるか？
+  def stock_ok?(drink_name)
+      if drink_name == "コーラ"
+          if (@cola[1] - 1) >= 0
+              @stocks = 0
+          else
+              puts "在庫がありません売り切れです！"
+              @stocks = 1
+          end
+      elsif drink_name == "レッドブル"
+          if (@redbull[1] - 1) >= 0
+              @stocks = 0
+          else
+              puts "在庫がありません売り切れです！"
+              @stocks = 1
+          end
+      elsif drink_name == "レッドブル"
+          if (@redbull[1] - 1) >= 0
+              @stocks = 0
+          else
+              puts "在庫がありません売り切れです！"
+              @stocks = 1
+          end
+      else
+          puts "そんな名前の飲み物は販売していません。"
+      end
   end
-
-  # 購入時：購入希望の飲み物の価格と投入額の計算.
-  # trueなら購入可
-  def purchase_cost(drink)
-    @total >= @drink_table[drink.name][:price]
+  #お金が足りるか確認
+  def moneyok?(drink_name)
+      if drink_name == "コーラ"
+          if (@total - 120) >= 0
+              @okmaney = 0
+          else
+              puts "投入金額が足りません"
+              @okmaney = 1
+          end
+      elsif drink_name == "レッドブル"
+          if (@total - 200) >= 0
+              @okmaney = 0
+          else
+              puts "投入金額が足りません"
+              @okmaney = 1
+          end
+      elsif drink_name == "レッドブル"
+          if (@total - 200) >= 0
+              @okmaney = 0
+          else
+              puts "投入金額が足りません"
+              @okmaney = 1
+          end
+      end
   end
-
-  # 購入時：飲み物の在庫確認
-  # trueなら購入可
-  def purchase_drink(drink)
-    @drink_table[drink.name][:drinks].size > 0
+  
+  #これ動いてない
+  # def money_sum(drink_name)
+  #     stock_price
+  #     if @okmaney == 0 and @stocks == 0
+  #         if drink_name == "cola"
+  #             @total = @total + @cola[0]
+  #         elsif drink_name == "水"
+  #             @total = @total + @water[0]
+  #         else
+  #             @total = @total + @redbull[0]
+  #         end
+  #     else
+  #         refund
+  #     end
+  # end
+  
+  #投入金額と在庫があれば処理が行われる
+  def purchase(drink_name)
+      if @okmaney == 0 and @stocks == 0
+          puts "#{drink_name}を取り出し口からお取りください"
+      else
+          refund
+      end
   end
-
-  # 飲み物の購入
-  def purchase(drink)
-    if purchase_drink(drink) && purchase_cost(drink)
-      drink = @drink_table[drink.name][:drinks].pop
-      @sale_amount += drink.price
-      @total -= drink.price
-      puts "購入した飲み物：#{drink.name}"
-      puts "購入金額：#{drink.price}"
-      refund
-    end
+  
+  #ここでいくつかの処理を同時に行うことができる
+  def buy(drink_name)
+      avilable_drink = ["コーラ", "レッドブル", "水"]
+      if avilable_drink.include?(drink_name)
+          stock_ok?(drink_name)
+          moneyok?(drink_name)
+          purchase(drink_name)
+          price(drink_name)
+          refund
+          stocks_down(drink_name)
+      else
+          puts "そんな名前の飲み物知らない。"
+          refund
+      end
   end
+  
+  
+  #ここでは自販機内部のお金のデータを保持します
+  def bank
+      @havemoney = 10000
+  end
+  
+  #購入可能の時に内部に売上金を加算
+  def price(drink_name)
+      bank
+      if @okmaney == 0 and @stocks == 0
+          if drink_name == "コーラ"
+              @havemoney = @havemoney + 120
+              @total = @total - 120
+          elsif drink_name == "レッドブル"
+              @havemoney = @havemoney + 200
+              @total = @total - 200
+          elsif drink_name == "水"
+              @havemoney = @havemoney + 100
+              @total = @total - 100
+          end
+      end
+  end
+  
+  #お金がどのくらいあるかわかる
+  def bank_info
+      puts "内部金庫には#{@havemoney}円あります。"
+  end
+  
+  #ここで商品の減算数
+  def stocks_down(drink_name)
+      stock_price
+      
+       if @okmaney == 0 and @stocks == 0
+          if drink_name == "コーラ"
+              @cola[1] = @cola[1] - 1
+          elsif drink_name == "レッドブル"
+              @redbull[1] = @redbull[1] - 1
+          elsif drink_name == "水"
+              @water = @water[1] - 1
+          end
+      end
+      
+      
+  end
+  
+  
 end
+nodokawaita = VendingMachine.new
+nodokawaita.info("コーラ")
+nodokawaita.total_money
+nodokawaita.insert(100)
+nodokawaita.insert(50)
+nodokawaita.buy("コーラ")
+nodokawaita.bank_info
+nodokawaita.info("コーラ")
+
+
+
